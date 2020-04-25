4b2de4e11604173d66a8c6ea589bc4bda99f28fd
Ubuntu <ubuntu@ip-172-31-56-136.ec2.internal>
2020-04-25 02:02:38 +0000

    encrypt

--- param.sh
+++ param.sh
@@ -1,4 +1,20 @@
-#!/bin/bash
-
-
-aws ssm put-parameter --name password --value MyPassword1233 --type SecureString --key-id 609c7713-936d-4dea-8d82-dc27d4c0a2a7 --region us-east-1
+{
+	"data": "ENC[AES256_GCM,data:LVM/qrm0043w264XdXgUuPI5IUAdDIlnkfSTgoWBu8906qBWKqoiLv+XHqB9wWABAFvsnZETe7N429YGgLbBBkZZj4ZrghlF4VdIJC9UREU+jTyGHdir0z23sMywXky5a0utS1nm/eosi2ic6uauXM0lxU0PNdqCgOYPKO/aoNGXAloHEXDR8Z7cfUrc1JWXBTCko4DsyV5KeRdnSEv2pA==,iv:wIYTSDe9in/wWrDUmNn29jEQc+zF5nq/l07jX1Ad1Zo=,tag:mkBBxFg20NTw4KIB5qSJ6w==,type:str]",
+	"sops": {
+		"kms": [
+			{
+				"arn": "arn:aws:kms:us-east-1:685704127483:key/609c7713-936d-4dea-8d82-dc27d4c0a2a7",
+				"created_at": "2020-04-25T02:02:16Z",
+				"enc": "AQICAHjtEMOoHTAfG1bNayt05lqXM1nc0PulPN05TSGFNq5aJwEqmKYhN/2MHhBqE9iVzphgAAAAfjB8BgkqhkiG9w0BBwagbzBtAgEAMGgGCSqGSIb3DQEHATAeBglghkgBZQMEAS4wEQQMCd6QgaY57pWCS35FAgEQgDt9Ajv9UIaBFV1bL0ZGyx0HPmEaI3QdYAoRHNXgIU/S30dDMarpLTIkB9Kj82HbioHK+Dlpj0mo6ZzMhQ==",
+				"aws_profile": ""
+			}
+		],
+		"gcp_kms": null,
+		"azure_kv": null,
+		"lastmodified": "2020-04-25T02:02:16Z",
+		"mac": "ENC[AES256_GCM,data:SeQRfy+X12vXUKFRAawK9e8tFWJLGQKu8EFyExXjROLoeFCO9UTQ7FU0VM5A6rLExaeeKOSJ3X6xjU2UatQWzyIQK7Lr3JxlZ2W1F/nt11I2sBxluqqvUqYhc9iTyUbcUioCZuDMi+TpNeC6EAc7qKpdNEBQz+5QJ7RKcg2p5RM=,iv:X3q8d4BZsXahhnZ95MBxc2e1KECe6o35+aQLhcpPJmA=,tag:pzO/qcbg06/ZHiUEnIhmfQ==,type:str]",
+		"pgp": null,
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.5.0"
+	}
+}
